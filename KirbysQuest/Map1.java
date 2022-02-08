480d13717676f0b59c7dfcbc16ae87b07204f16d
karenlyuan <65468255+karenlyuan@users.noreply.github.com>
2022-02-08 08:43:16 -0800

    Create Map1.java

---
+++ KirbysQuest/Map1.java
@@ -0,0 +1,19 @@
+public class Map1 {
+	
+	private int[][] board;
+	private int kX, kY; // Kirby location
+	private int cX, cY; // cake location
+	private int dX, dY; // door location
+	
+	
+	public Map1() {
+		board = new int[4][4];
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
