10f9d9b0246d44ca78ec51f62926ecedd875a8ea
karenlyuan <65468255+karenlyuan@users.noreply.github.com>
2022-03-04 09:19:24 -0800

    Update p1.java

--- KirbysQuest/p1.java
+++ KirbysQuest/p1.java
@@ -5,12 +5,12 @@ public class p1 {
 	
 	private static int rows, cols;
 	private static int rooms;
+	
+	static Scanner scanner;
+	static File fMap = new File("Map1");
+	static File fRoute = new File("Map1Route");
 
 	public static void main(String[] args) {
-		
-		Scanner scanner;
-		File f = new File("Map1");
-		File fRoute = new File("Map1Route");
 		
 		/* two output formats
 		 * 
@@ -26,97 +26,115 @@ public class p1 {
 		 * 
 		 */
 		
-		System.out.println(f); //original maze setup
-		System.out.println(fRoute); //route Kirby takes
-		
-		
-		try {
-			//code that might throw a special error
-			scanner = new Scanner(fRoute);
-			
-			//use next methods to grab the first 3 numbers
-			//from the file for your map info
-			
-			int rows = scanner.nextInt();
-			int cols = scanner.nextInt();
-			int rooms = scanner.nextInt();
-			
-			String[][] map = new String[rows][cols];
-			
-			for(int r=0; r<rows; r++) {
-				for(int c=0; c<cols; c++) {
-					map[r][c] = scanner.next();
-				}
-				
-				scanner.nextLine();
-			}
-			
-			for(int i=0; i<rows; i++) {
-				int count = 0;
-				for(int j=0; j<cols; j++) {
-					if(scanner.hasNext("+")) {
-						//set first column first row a "+" sign and its coordinates
-						System.out.println("+ " + i + " " + j);
-					} else if(!scanner.hasNext("+")) {
-						System.out.println();
-					}
-					if(scanner.hasNext("C")) {
-						count++;
-					}
-				}
-					
-				scanner.nextLine();
-				if(count != 1) {
-					System.out.println("The cake is a lie.");
-				}
-				
-			} //prints coordinate-based solution of Kirby's route
-			
-			scanner.nextLine(); //move scanner to next line
-			while(scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				
-				String num = line.charAt(2)+"";
-				
-				//use charAt to grab the element of the map for a given row
-				System.out.println(line);
-				System.out.println(line.charAt(1));
-				
-				//be able to read in a map-based input in a way that each
-				// element of the map is accessible in some easy way
-				
-			}
-			
-			
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		
-		/*public void coordinateBased(Scanner scan) {
-			rows = scan.nextInt();
-			cols = scan.nextInt();
-			rooms = scan.nextInt();
-			
-			scan.nextLine();
-			
-			while(scanner.hasNextLine()) {
-				Integer.valueOf(rows);
-				scan.next();
-			}
-			
-			
-		}
-		
-
+		mapBased(scanner);
+		coordinateBased(scanner);
 	}
+	
+		public static void mapBased(Scanner scan) {
+			try {
+				scan = new Scanner(fMap);
+				
+				int rows = scan.nextInt();
+				int cols = scan.nextInt();
+				int rooms = scan.nextInt();
+				
+				String[][] map = new String[rows][cols];
+				
+				for(int r=0; r<rows; r++) {
+					for(int c=0; c<cols; c++) {
+						map[r][c] = String.valueOf(scan.next());
+						System.out.print(map[r][c]);
+					}
+					System.out.println();
+					scan.nextLine();
+				}
+				
+				//scan.nextLine(); //move scanner to next line
+//				while(scan.hasNextLine()) {
+//					String line = scan.nextLine();
+//					
+//					String num = line.charAt(2)+"";
+//					
+//					//use charAt to grab the element of the map for a given row
+//					System.out.println(line);
+//					System.out.println(line.charAt(1));
+//					
+//					//be able to read in a map-based input in a way that each
+//					// element of the map is accessible in some easy way
+//					
+//				}
+				
+				
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		
+		
+		public static void coordinateBased(Scanner scan) {
+//			scan.nextLine();
+//			
+//			while(scanner.hasNextLine()) {
+//				Integer.valueOf(rows);
+//				scan.next();
+//			}
+			
+			try {
+				//code that might throw a special error
+				scan = new Scanner(fRoute);
+				
+				//use next methods to grab the first 3 numbers
+				//from the file for your map info
+				
+				int rows = scan.nextInt();
+				int cols = scan.nextInt();
+				int rooms = scan.nextInt();
+				
+				String[][] cMap = new String[rows][cols];
+				
+				for(int r=0; r<rows; r++) {
+					for(int c=0; c<cols; c++) {
+						cMap[r][c] = String.valueOf(scan.nextInt());
+					}
+					
+					scan.nextLine();
+				}
+				
+				for(int i=0; i<rows; i++) {
+					int count = 0;
+					for(int j=0; j<cols; j++) {
+						if(scan.next() == "+") {
+							//set first column first row a "+" sign and its coordinates
+							System.out.print("+ " + i + " " + j);
+						} else if(scan.next() != "+") {
+							System.out.print(cMap[i][j] + " " + i + " " + j);
+						}
+						if(scan.hasNext("C")) {
+							count++;
+						}
+					}
+					
+					System.out.println();
+					scan.nextLine();
+					
+					if(count != 1) {
+						System.out.println("The cake is a lie.");
+					}
+					
+				}
+				
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			
+	}
+}
+		
 
-	private static char[] charAt(int i) {
+
+	/* private static char[] charAt(int i) {
 		if(i < 0 || i > rows) {
 			throw new StringIndexOutOfBoundsException(i);
 		}
 		return rows[i];
 	} */
-
-	}
-
-}
