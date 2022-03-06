3d7fce04ad6cb07502e9a1d7586a76e3c22d5cb9
karenlyuan <65468255+karenlyuan@users.noreply.github.com>
2022-03-06 01:40:40 -0800

    Update p1.java

--- KirbysQuest/p1.java
+++ KirbysQuest/p1.java
@@ -7,7 +7,7 @@ public class p1 {
 	private static int rooms;
 	
 	static Scanner scanner;
-	static File fMap = new File("Map1");
+	static File fMap = new File("Map1Route");
 	static File fRoute = new File("Map1Route");
 
 	public static void main(String[] args) {
@@ -26,9 +26,14 @@ public class p1 {
 		 * 
 		 */
 		
+		
+		System.out.println("Map based:");
 		mapBased(scanner);
+		System.out.println();
+		System.out.println("Coordinate based:");
 		coordinateBased(scanner);
 	}
+	
 	
 		public static void mapBased(Scanner scan) {
 			try {
@@ -38,31 +43,20 @@ public class p1 {
 				int cols = scan.nextInt();
 				int rooms = scan.nextInt();
 				
+				System.out.println(rows+" "+cols+" "+rooms);
+				
 				String[][] map = new String[rows][cols];
 				
-				for(int r=0; r<rows; r++) {
-					for(int c=0; c<cols; c++) {
-						map[r][c] = String.valueOf(scan.next());
-						System.out.print(map[r][c]);
+				while(scan.hasNextLine()) {
+					for(int r=0; r<rows; r++) {
+						for(int c=0; c<cols; c++) {
+							map[r][c] = String.valueOf(scan.next());
+							System.out.print(map[r][c]+" ");
+						}
+						System.out.println();
+						scan.nextLine();
 					}
-					System.out.println();
-					scan.nextLine();
 				}
-				
-				//scan.nextLine(); //move scanner to next line
-//				while(scan.hasNextLine()) {
-//					String line = scan.nextLine();
-//					
-//					String num = line.charAt(2)+"";
-//					
-//					//use charAt to grab the element of the map for a given row
-//					System.out.println(line);
-//					System.out.println(line.charAt(1));
-//					
-//					//be able to read in a map-based input in a way that each
-//					// element of the map is accessible in some easy way
-//					
-//				}
 				
 				
 			} catch (Exception e) {
@@ -90,37 +84,61 @@ public class p1 {
 				int cols = scan.nextInt();
 				int rooms = scan.nextInt();
 				
+				//System.out.println("rows: "+rows+" cols: "+cols+" rooms: "+rooms);
+				// rows: 4 cols: 4 rooms: 1
+				
 				String[][] cMap = new String[rows][cols];
+				
+				int count = 0;
 				
 				for(int r=0; r<rows; r++) {
 					for(int c=0; c<cols; c++) {
-						cMap[r][c] = String.valueOf(scan.nextInt());
-					}
-					
-					scan.nextLine();
-				}
-				
-				for(int i=0; i<rows; i++) {
-					int count = 0;
-					for(int j=0; j<cols; j++) {
-						if(scan.next() == "+") {
-							//set first column first row a "+" sign and its coordinates
-							System.out.print("+ " + i + " " + j);
-						} else if(scan.next() != "+") {
-							System.out.print(cMap[i][j] + " " + i + " " + j);
-						}
-						if(scan.hasNext("C")) {
+//						String line = scan.nextLine();
+//						if(scan.hasNext("+")) {
+//							System.out.println("+"+" "+r+" "+c);
+//						}
+						
+						//String line = scan.nextLine();
+						//System.out.println(line.charAt(c)+" "+r+" "+c);
+						cMap[r][c] = String.valueOf(scan.next());
+						System.out.println(cMap[r][c]+" "+r+" "+c);
+						if(scan.next() == "C") {
 							count++;
 						}
 					}
 					
-					System.out.println();
 					scan.nextLine();
 					
-					if(count != 1) {
-						System.out.println("The cake is a lie.");
 					}
-					
+				
+//				while(scan.hasNextLine()) {
+//					String line = scan.nextLine();
+//					
+//					String num = line.charAt(2)+"";
+//					
+//					//use charAt to grab the element of the map for a given row
+//					System.out.println(line);
+//					System.out.println(line.charAt(1));
+//					
+//				}
+				
+//				for(int i=0; i<rows; i++) {
+//					for(int j=0; j<cols; j++) {
+//						if(scan.next() == "+") {
+//							//set first column first row a "+" sign and its coordinates
+//							System.out.print("+ " + i + " " + j);
+//						} else if(scan.next() != "+") {
+//							System.out.print(cMap[i][j] + " " + i + " " + j);
+//						}
+//					}
+//					
+//					System.out.println();
+//					scan.nextLine();
+//					
+//				}
+				
+				if(count != 1) {
+					System.out.println("The cake is a lie.");
 				}
 				
 			} catch (Exception e) {
