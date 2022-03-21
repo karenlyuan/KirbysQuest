0a5f3f18440b6841a89c1f37cda0e511af5bad99
karenlyuan <65468255+karenlyuan@users.noreply.github.com>
2022-03-20 23:47:02 -0700

    Update p1.java

--- KirbysQuest/p1.java
+++ KirbysQuest/p1.java
@@ -2,10 +2,19 @@ public class p1 {
   
 	public static void main(String[] args) {
 		System.out.println("Map based:");
-		Formats.mapBased(Formats.scanner);
+		Formats.mapBased(Formats.scanner, Formats.fMap);
 		System.out.println();
 		System.out.println("Coordinate based:");
-		Formats.coordinateBased(Formats.scanner);
+		Formats.coordinateBased(Formats.scanner, Formats.fRoute);
+		
+		//print out queue route
+		Queue.algorithm();
+		
+		//print out stack route
+		
+		
+		//print out optimal path route
+		
 		
 	}
 
